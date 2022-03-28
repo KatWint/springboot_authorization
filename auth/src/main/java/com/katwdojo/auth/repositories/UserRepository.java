8917bc56f46ff8fe904307a2d7f5cefacc49072a
Kathryn Wint <92839131+KatWint@users.noreply.github.com>
2022-03-27 20:36:30 -0700

    first commit

---
+++ auth/src/main/java/com/katwdojo/auth/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.katwdojo.auth.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.katwdojo.auth.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+ User findByUsername(String username);
+}
+
+
